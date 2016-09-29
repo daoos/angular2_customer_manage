530c388c6f14618ddf0e3260c6c63e2855c4be13
daoos <igeeker@126.com>
2016-09-29 13:05:09 +0800

    更改

--- systemjs.config.js
+++ systemjs.config.js
@@ -6,7 +6,7 @@
     // map tells the System loader where to look for things
     //配置相应资源的映射路径
     var map = {
-        'app': 'app', // 'dist',
+        'app': 'build',//'app', // 'dist',
         '@angular': 'node_modules/@angular',
         'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
         'rxjs': 'node_modules/rxjs'
@@ -14,7 +14,8 @@
     // packages tells the System loader how to load when no filename and/or no extension
     //
     var packages = {
-        'app': {main: 'main.js', defaultExtension: 'js'},
+        //'app': {main: 'main.js', defaultExtension: 'js'},
+        'build': {main: 'main.js', defaultExtension: 'js'},
         'rxjs': {defaultExtension: 'js'},
         'angular2-in-memory-web-api': {main: 'index.js', defaultExtension: 'js'},
     };
